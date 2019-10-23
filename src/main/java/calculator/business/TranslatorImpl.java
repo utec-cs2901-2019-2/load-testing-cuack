6e4ae33e436a873a6d0bd6d2f366ad673d5899bf
dgcnz <canezdiego@gmail.com>
2019-10-23 12:19:40 -0500

    Added preprocess function

--- src/main/java/calculator/business/TranslatorImpl.java
+++ src/main/java/calculator/business/TranslatorImpl.java
@@ -12,8 +12,15 @@ public class TranslatorImpl implements Translator {
 		apiKey = dotenv.get("API_KEY");
 	}
 
+    public String preprocess(String text) {
+        String[] tokens = text.split("\\s");
+        for (int i = 0; i < tokens.length; i++) {
+            tokens[i] = tokens[i].toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        }
+        return String.join(" ", tokens);
+    }
+
     @Override
     public String translate(Language from, Language to, String text) {
-        return "Hola Mundo";
     }
 }
