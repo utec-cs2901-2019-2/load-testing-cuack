06bc2552aaf2bc776daf6e095904416e388841b0
pieromarini <piero.marini21@gmail.com>
2019-10-23 12:19:41 -0500

    Created function to generate the request url

--- src/main/java/translator/business/TranslatorImpl.java
+++ src/main/java/translator/business/TranslatorImpl.java
@@ -1,19 +1,28 @@
 package translator.business;
 
-import translator.entities.Language;
 import io.github.cdimascio.dotenv.Dotenv;
+import translator.entities.Language;
 
 public class TranslatorImpl implements Translator {
 
-	private String apiKey;
+  private String apiKey;
 
-	public void loadEnv() {
-		Dotenv dotenv = Dotenv.load();
-		apiKey = dotenv.get("API_KEY");
-	}
+  TranslatorImpl() {
+    loadEnv();
+  }
 
-    @Override
-    public String translate(Language from, Language to, String text) {
-        return "Hola Mundo";
-    }
+  private void loadEnv() {
+    Dotenv dotenv = Dotenv.load();
+    apiKey = dotenv.get("API_KEY");
+  }
+
+  private String getRequestUrl(String text, String langFrom, String langTo) {
+    return String.format(
+        "https://translate.yandex.net/api/v1.5/tr.json/translate?key=%s&text=%s&lang=%s-%s",
+        apiKey, text, langFrom, langTo);
+  }
+
+  public String translate(Language from, Language to, String text) {
+    return "Hola Mundo";
+  }
 }
